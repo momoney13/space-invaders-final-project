c401ad16630cb36ab009ab2b1121f39f810268ea
zhukv98 <76707463+zhukv98@users.noreply.github.com>
2021-08-06 14:49:47 -0400

    fixing the highscores.js

--- highscores.js
+++ highscores.js
@@ -1,56 +1,57 @@
-// var gameSection = document.querySelector("section#game");
-// var high_scores = gameSection.querySelector("section#game ol.high-scores");
-// function HighScores() {
-//     if (typeof (Storage) !== "undefined") {
-//         var scores = false;
-//         if (localStorage["high-scores"]) {
-//             high_scores.style.display = "block";
-//             high_scores.innerHTML = '';
-//             scores = JSON.parse(localStorage["high-scores"]);
-//             scores = scores.sort(function (a, b) { return parseInt(b) - parseInt(a) });
+var gameSection = document.getElementById("game");
+var start = gameSection.getElementsByClassName("start");
+var high_scores = start[0];
+function HighScores() {
+    if (typeof (Storage) !== "undefined") {
+        var scores = false;
+        if (localStorage["high-scores"]) {
+            high_scores.style.display = "block";
+            high_scores.innerHTML = '';
+            scores = JSON.parse(localStorage["high-scores"]);
+            scores = scores.sort(function (a, b) { return parseInt(b) - parseInt(a) });
 
-//             for (var i = 0; i < 10; i++) {
-//                 var s = scores[i];
-//                 var fragment = document.createElement('li');
-//                 fragment.innerHTML = (typeof (s) != "undefined" ? s : "");
-//                 high_scores.appendChild(fragment);
-//             }
-//         }
-//     } else {
-//         high_scores.style.display = "none";
-//     }
-// }
-// function UpdateScore(score) {
-//     if (typeof (Storage) !== "undefined") {
-//         var current = parseInt(score);
-//         var scores = false;
-//         var player = prompt("Please enter your name", "")
-//         if (player != null) {
-//             if (localStorage["high-scores"]) {
+            for (var i = 0; i < 10; i++) {
+                var s = scores[i];
+                var fragment = document.createElement('li');
+                fragment.innerHTML = (typeof (s) != "undefined" ? s : "");
+                high_scores.appendChild(fragment);
+            }
+        }
+    } else {
+        high_scores.style.display = "none";
+    }
+}
+function UpdateScore(score) {
+    if (typeof (Storage) !== "undefined") {
+        var current = parseInt(score);
+        var scores = false;
+        var player = prompt("Please enter your name", "")
+        if (player != null) {
+            if (localStorage["high-scores"]) {
 
-//                 scores = JSON.parse(localStorage["high-scores"]);
-//                 scores = scores.sort(function (a, b) { return parseInt(b) - parseInt(a) });
+                scores = JSON.parse(localStorage["high-scores"]);
+                scores = scores.sort(function (a, b) { return parseInt(b) - parseInt(a) });
 
-//                 for (var i = 0; i < 10; i++) {
-//                     var s = parseInt(scores[i]);
+                for (var i = 0; i < 10; i++) {
+                    var s = parseInt(scores[i]);
 
-//                     var val = (!isNaN(s) ? s : 0);
-//                     if (current > val) {
-//                         val = current;
-//                         scores.splice(i, 0, player + "     " + parseInt(current));
-//                         break;
-//                     }
-//                 }
+                    var val = (!isNaN(s) ? s : 0);
+                    if (current > val) {
+                        val = current;
+                        scores.splice(i, 0, player + "     " + parseInt(current));
+                        break;
+                    }
+                }
 
-//                 scores.length = 10;
-//                 localStorage["high-scores"] = JSON.stringify(scores);
+                scores.length = 10;
+                localStorage["high-scores"] = JSON.stringify(scores);
 
-//             } else {
-//                 var scores = new Array();
-//                 scores[0] = current;
-//                 localStorage["high-scores"] = JSON.stringify(scores);
-//             }
-//             HighScores();
-//         }
-//     }
-// }
+            } else {
+                var scores = new Array();
+                scores[0] = current;
+                localStorage["high-scores"] = JSON.stringify(scores);
+            }
+            HighScores();
+        }
+    }
+}
